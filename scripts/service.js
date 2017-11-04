c379f9779cd9acaa9c2266552d12af06c123e2f3
Vladick25 <tolmasoft@bk.ru>
2017-11-04 22:23:08 +0200

    sdfghj

--- scripts/service.js
+++ scripts/service.js
@@ -8,7 +8,7 @@ function createTopSlot(){
 	var upperDiv = document.createElement('div');
 	upperDiv.style.position = 'absolute'; 
     upperDiv.style.cursor = 'pointer';
-    upperDiv.style.z-index = '2';
+    upperDiv.style['z-index'] = '2';
 
     var bg = document.createElement('img');
     bg.src = "../images/slot_top.png";
