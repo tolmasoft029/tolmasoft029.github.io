891be26284bdde266f6c042a9aca3b6bad3cb32d
Vladick25 <tolmasoft@bk.ru>
2017-11-04 22:08:33 +0200

    dffgn

--- scripts/service.js
+++ scripts/service.js
@@ -1,3 +1,21 @@
+var top_slot = {};
+
+createTopSlot();
+
 function animImage(MyImage, src) {
     MyImage.src = src;
+}
+
+function createTopSlot(){
+	var upperDiv = document.createElement('div');
+	div.style.position = 'absolute'; 
+    div.style.cursor = 'pointer';
+
+    var bg = upperDiv.createElement('img');
+    bg.src = "../images/slot_top.png";
+    bg.className = "locked";
+
+    top_slot = upperDiv;
+
+    document.body.appendChild(upperDiv);
 }
