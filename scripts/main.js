5a133e1b84273f437983acf0012b7c7d38c943e4
Vladick25 <tolmasoft@bk.ru>
2017-11-04 16:54:15 +0200

    test mask

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,9 @@
-var params = window
-    .location
-    .search
-    .replace('?','')
-    .split('&')
-    .reduce(
-        function(p,e){
-            var a = e.split('=');
-            p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
-            return p;
-        },
-        {}
-    );
+var params = window.location.search.replace('?','').split('&').reduce(function(p,e){var a = e.split('=');
+									p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);return p;},{});
 
 VK.init(onInit, onError, '5.69'); 
 
 function onInit(){
-	
-    alert(params['viewer_id']);
 	VK.api("users.get", {user_ids:"112354918", fields:"photo_50"}, onPhoto);
 }
 
